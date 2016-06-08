00d65bc45cedb1a1a9341095212e84477ba32c96
Ruslan Kiricenko <ruslankus@yahoo.com>
2016-06-08 23:48:38 +0300

    last update

--- js/common.js
+++ js/common.js
@@ -21,7 +21,31 @@ $(function() {
 		
 		return false;
 	});//click
-	
+
+
+	$("#sendMsg").click(function(){
+		
+		var spinner = createSpinner();
+
+		$('#formHolder').append(spinner);
+		//collectiong form data
+
+		var formData = $('.form').serializeArray();
+
+		console.log(formData);
+
+		
+
+
+		setTimeout(function() {
+
+			fadeSpinner();
+
+		}, 1000);
+
+
+		return false;
+	});//click sendMsg
 	
 	
    
@@ -35,4 +59,34 @@ function checkPos(){
 		$('#nav-holder').removeClass('move');
 	}
 }//checkPos
+
+
+function createSpinner(){
+	
+	var spinnerHolder = $("<div/>").attr('id','spinner-holder');	
+	var spinsHolder = $("<div/>").attr('id','circularG');
+	
+	for(i=1;i < 9; i++){
+	
+		var el = $("<div/>").attr('id', ("circularG_" + i)).addClass("circularG");	
+		spinsHolder.append(el);				
+	}
+	var td = $("<td/>").append(spinsHolder); 
+	var tr = $("<tr>").append(td);
+	var table = $("<table/>").append(tr);
+	
+	spinnerHolder.append(table);
+	console.log(spinnerHolder)
+	return spinnerHolder;
+}//createspinner
+
+
+function fadeSpinner()
+{
+	console.log("fade")
+	$("#spinner-holder").fadeIn('slow',function(){
+		console.log($(this));
+		$(this).remove();
+	});
+}
 
